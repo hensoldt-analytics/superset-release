ddfaa53843701a2a47c0ee65ed8f5402492eaceb
Vi On <von@hortonworks.com>
2019-04-18 11:24:11 -0700

    BUG-119654 fix superset defusedxml = 0.5.0
    Change-Id: I04e16fecf22b52bcf9b9c7928419c4f5d4a22af4
    (cherry picked from commit ba3a56281b9b695a39b225339b735ee393b00cde)
    (cherry picked from commit a15b94285352bf4b020faef5afef56d95ff944d7)

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     install_requires=[
         'celery==3.1.23',
         'cryptography==1.5.3',
+        'defusedxml==0.5.0',
         'flask==0.12.2',
         'flask-appbuilder==1.8.1',
         'flask-cache==0.13.1',
