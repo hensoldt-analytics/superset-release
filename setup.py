41b6c8854e0276380ad971ef6aa2d928307c7a41
Jenkins <jenkins@hortonworks.com>
2020-12-07 15:59:55 +0530

    HOTFIX-3685 fixing the superset failure.
    Change-Id: I8747f8031a92a8702382defbdf50f0746612ca9c

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'python-dateutil==2.5.3',
         'requests==2.10.0',
         'simplejson==3.8.2',
-        'six==1.10.0',
+        'six>=1.10.0',
         'sqlalchemy==1.1.5',
         'sqlalchemy-utils==0.32.7',
         'sqlparse==0.1.19',
